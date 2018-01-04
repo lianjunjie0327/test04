3d2d069efa5795f9a2aedb483645d80979ec6890
Administrator <Administrator@73Q4K31HY602G2V>
2018-01-04 21:45:11 +0800

    4

--- src/com/zhiyou100/demo/Demo.java
+++ src/com/zhiyou100/demo/Demo.java
@@ -3,8 +3,7 @@ package com.zhiyou100.demo;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println(1);
-		System.out.println(1);
-		System.out.println(1);
+		System.out.println(2);
+		
 	}
 }
